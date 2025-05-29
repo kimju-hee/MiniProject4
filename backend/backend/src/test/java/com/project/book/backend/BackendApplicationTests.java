b24ef7650957f6a223f1842196f80906a5104707
김주희 <lsj240606@naver.com>
2025-05-29 16:06:02 +0900

    feat: 초기 프로젝트 세팅

---
+++ backend/backend/src/test/java/com/project/book/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.book.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
