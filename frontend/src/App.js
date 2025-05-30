7251adba38f04b102e841d4dff539ab66e3ace80
egoing <egoing@gmail.com>
2025-05-30 10:43:36 +0900

    main+UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import BookRegister from './pages/BookRegister';
+import Book_Page from './pages/Book_Page';
+import Book_Edit from './pages/Book_Edit';
 
 function App() {
   return (
@@ -16,6 +21,17 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
+          <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        {/* 나중에 도서 등록/상세 페이지 경로도 여기에 추가 가능 */}
+        <Route path="/books/register" element={<BookRegister />} />      
+        <Route path="/books/:id" element={<Book_Page />} />
+        <Route path="/books/edit/:id" element={<Book_Edit />} />
+
+      </Routes>
+    </BrowserRouter>
         </a>
       </header>
     </div>
