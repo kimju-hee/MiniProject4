1f79b2b7c64991716af2b8f66ef26f0c3edfe4f5
egoing <egoing@gmail.com>
2025-05-30 11:12:55 +0900

    로고 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import MainPage from './pages/MainPage';
 import BookRegister from './pages/BookRegister';
 import Book_Page from './pages/Book_Page';
 import Book_Edit from './pages/Book_Edit';
+import React, { useEffect } from 'react';
 
 function App() {
     useEffect(() => {
@@ -14,21 +15,7 @@ function App() {
     }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-
-          <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         {/* 나중에 도서 등록/상세 페이지 경로도 여기에 추가 가능 */}
@@ -38,9 +25,6 @@ function App() {
 
       </Routes>
     </BrowserRouter>
-        </a>
-      </header>
-    </div>
   );
 }
 
